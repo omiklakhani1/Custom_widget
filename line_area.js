2b32c91ed723d034b85c287e8252c1fae80d3884
omiklakhani1 <116053903+omiklakhani1@users.noreply.github.com>
2022-11-15 11:01:32 +0530

    Update line_area.js

--- line_area.js
+++ line_area.js
@@ -9,8 +9,40 @@
 	//Chart Block in HTML
 	const prepared = document.createElement('template')
 	prepared.innerHTML = `
-		<div id="root" style="width: 100%; height: 100%;">
-		</div>
+	
+	<style>
+	html,body
+	{
+		height: 100%;
+		margin: 0;
+	}
+	
+	#root{
+	   background-color: white;
+	}
+
+	#placeholder {
+	   padding-top: 1em;
+	   text-align: center;
+	   font_size: 1.5em;
+	   color: black;
+	}
+
+	#chatdiv {
+	   width: 100%;
+	   height: 98%;
+
+	   flex-diction: column;
+	   display: -webkit-box;
+	   display: -ms-box;
+	   display: -ms-flexbox;
+	   display: -webkit-flet;
+	   display: flex;
+	   
+	}
+	 </style>
+	 <div id="root" style="width: 100%; height: 100px;">
+	 </div>
 	  `
 	//Main JS Class holds methods to be called
 	class SamplePrepared extends HTMLElement {
@@ -82,68 +114,84 @@
 	  //render() method to plot chart - resultSet1 holds data from SAC table/chart.
 	  async render(resultset) {
 
+		this._placeholder = this._root.querySelector('#placeholder')
+		if(this._placeholder){
+			this._root.removeChild(this._placeholder)
+			this._placeholder = null
+		}
+
+		var mychartdiv =  this._shadowRoot.getElementById('chartdiv')
+
+		am4core.ready(function() {	var finaldata = [];
+
+			for(var i = 0 ; i < resultset.length; i++){
+				finaldata.push({ date: resultset[i].date.id, value: resultset[i]["@MeasureDimension"].rawValue});
+			}
+	
+			// for(var i = 0 ; i < resultset.length; i++){
+			// 	finaldata.push({ date: resultset[i].date.id, value: resultset[i]["@MeasureDimension"].rawValue});
+			// }
+	
+			console.log(resultset);
+		   
+			console.log(finaldata);
+	
+			
+			// Themes begin
+			am4core.useTheme(am4themes_animated);
+			// Themes end
+			
+			
+			var chart = am4core.create(this._root, am4charts.XYChart);
+	  
+			// for(var i = 0 ; i < resultset.length; i++){
+				// finaldata.push({ date: resultset[i].Ship_Date.id, value: resultset[i]["@MeasureDimension"].rawValue});
+			// }
+			
+			// var data = [];
+			// var value = 50;
+			// for (var i = 0; i < 300; i++) {
+			//   var date = new Date();
+			//   date.setHours(0, 0, 0, 0);
+			//   date.setDate(i);
+			//   value -= Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 10);
+			//   data.push({ date: date, value: value });
+			// }
+			chart.data = finaldata;
+	
+	  
+			// Create axes
+			var dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+			dateAxis.renderer.minGridDistance = 60;
+	  
+			var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+	  
+			// Create series
+			var series = chart.series.push(new am4charts.LineSeries());
+			series.dataFields.valueY = "value";
+			series.dataFields.dateX = "date";
+			series.tooltipText = "{value}"
+	  
+			series.tooltip.pointerOrientation = "vertical";
+	  
+			chart.cursor = new am4charts.XYCursor();
+			chart.cursor.snapToSeries = series;
+			chart.cursor.xAxis = dateAxis;
+	  
+
+		});
+	
+		
+
+
+
 
 
 		// await getScriptPromisify('https://cdn.amcharts.com/lib/4/core.js');
 		// await getScriptPromisify('https://cdn.amcharts.com/lib/4/themes/animated.js');
 		// await getScriptPromisify('https://cdn.amcharts.com/lib/4/charts.js');
 
-		var finaldata = [];
-
-		for(var i = 0 ; i < resultset.length; i++){
-			finaldata.push({ date: resultset[i].date.id, value: resultset[i]["@MeasureDimension"].rawValue});
-		}
-
-		// for(var i = 0 ; i < resultset.length; i++){
-		// 	finaldata.push({ date: resultset[i].date.id, value: resultset[i]["@MeasureDimension"].rawValue});
-		// }
-
-		console.log(resultset);
-       
-		console.log(finaldata);
-
-		
-		// Themes begin
-		am4core.useTheme(am4themes_animated);
-		// Themes end
-		
-		
-		var chart = am4core.create(this._root, am4charts.XYChart);
-  
-		// for(var i = 0 ; i < resultset.length; i++){
-			// finaldata.push({ date: resultset[i].Ship_Date.id, value: resultset[i]["@MeasureDimension"].rawValue});
-		// }
-		
-		// var data = [];
-		// var value = 50;
-		// for (var i = 0; i < 300; i++) {
-		//   var date = new Date();
-		//   date.setHours(0, 0, 0, 0);
-		//   date.setDate(i);
-		//   value -= Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 10);
-		//   data.push({ date: date, value: value });
-		// }
-		chart.data = finaldata;
-
-  
-		// Create axes
-		var dateAxis = chart.xAxes.push(new am4charts.DateAxis());
-		dateAxis.renderer.minGridDistance = 60;
-  
-		var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
-  
-		// Create series
-		var series = chart.series.push(new am4charts.LineSeries());
-		series.dataFields.valueY = "value";
-		series.dataFields.dateX = "date";
-		series.tooltipText = "{value}"
-  
-		series.tooltip.pointerOrientation = "vertical";
-  
-		chart.cursor = new am4charts.XYCursor();
-		chart.cursor.snapToSeries = series;
-		chart.cursor.xAxis = dateAxis;
-  
+	
   
 	  }
 	
